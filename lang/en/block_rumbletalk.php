bba0bf10a96be0f2956765be503deaf840b03b25
Jeirod Abogado <33385627+jeirod@users.noreply.github.com>
2021-07-15 06:30:41 +0800

    docs: add additional strings for the addHelpButton

--- lang/en/block_rumbletalk.php
+++ lang/en/block_rumbletalk.php
@@ -8,10 +8,30 @@ $string['create_email'] = 'Email: ';
 $string['create_password'] = 'Password: ';
 $string['create_button'] = 'Create';
 
-// Embed Strings
+// Chat: Embed Strings
 $string['embed_chat'] = 'Embed a Chat';
 $string['embed_height'] = 'Chat Height: ';
 $string['embed_code'] = 'Chat Hashcode: ';
+$string['code'] = 'How to get hashcode?';
+$string['code_help'] = '<p>Where is a hashcode found?</p> 
+<p>You can find it at you Administrator panel(https://cp.rumbletalk.com/login) at RumbleTalk.</p>';
+
+// Chat: Height String
+$string['height'] = 'What is Chat Height?';
+$string['height_help'] = '<p>Chat\'s Height is based on pixels(px).</p>
+<p>Please enter numbers only.</p>';
+
+// Chat: Members String
+$string['members'] = 'Read instructions first, before checking the box.';
+$string['members_help'] = '<p>Before checking the box, you must first follow the steps below:</p>
+<ol>
+    <li>Login to your Administrator Panel(https://cp.rumbletalk.com/login)</li>
+    <li>Choose your chat at "Selected Chat"</li>
+    <li>Open "Settings"</li>
+    <li>Select "General & Logins" at the drop-down list</li>
+    <li>Click "Login Options" at the General tab</li>
+    <li>Check the box for "User authentication required"</li>
+</ol>';
 
 // Settings Strings
 $string['headerconfig'] = 'Settings';
