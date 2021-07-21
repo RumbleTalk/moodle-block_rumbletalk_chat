0e1d924146153b9b6c5d5b84ea1cec00486cc7a0
Jeirod Abogado <33385627+jeirod@users.noreply.github.com>
2021-07-22 02:42:27 +0800

    docs: updated copyright and change block name

--- create_account_form.php
+++ create_account_form.php
@@ -16,9 +16,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Handles displaying the rumbletalk block.
+ * Handles displaying the rumbletalk group chat block.
  *
- * @package    block_rumbletalk
+ * @package    block_rumbletalk_chat
  * @copyright  2021 RumbleTalk, LTD {@link https://www.rumbletalk.com/}
  * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
@@ -38,21 +38,21 @@ class create_account_form extends \moodleform {
         $this->user = $this->_customdata['user'];
         $this->course = $this->_customdata['course'];
 
-        $mform->addElement('header', 'config_header', get_string('create_account', 'block_rumbletalk'));
+        $mform->addElement('header', 'config_header', get_string('create_account', 'block_rumbletalk_chat'));
  
         // A sample string variable with a default value.
-        $mform->addElement('text', 'create_email', get_string('create_email', 'block_rumbletalk'), 'minlenght="10" maxlength="100" size="25"');
-        $mform->addRule('create_email', get_string('error_email_required', 'block_rumbletalk'), 'required', null, 'client');
-        // $mform->addRule('create_email', get_string('error_email_regex', 'block_rumbletalk'), 'regex', '^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$', 'client');
+        $mform->addElement('text', 'create_email', get_string('create_email', 'block_rumbletalk_chat'), 'minlenght="10" maxlength="100" size="25"');
+        $mform->addRule('create_email', get_string('error_email_required', 'block_rumbletalk_chat'), 'required', null, 'client');
+        // $mform->addRule('create_email', get_string('error_email_regex', 'block_rumbletalk_chat'), 'regex', '^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$', 'client');
         $mform->setType('create_email', PARAM_NOTAGS);
 
         // A sample string variable with a default value.
-        $mform->addElement('password', 'create_password', get_string('create_password', 'block_rumbletalk'), 'minlength="6" maxlength="12" size="25"');
-        $mform->addRule('create_password', get_string('error_password_required', 'block_rumbletalk'), 'required', null, 'client');
-        // $mform->addRule('create_password', get_string('error_password_regex', 'block_rumbletalk'), 'regex', '^[^,]{6,12}$', 'client');
+        $mform->addElement('password', 'create_password', get_string('create_password', 'block_rumbletalk_chat'), 'minlength="6" maxlength="12" size="25"');
+        $mform->addRule('create_password', get_string('error_password_required', 'block_rumbletalk_chat'), 'required', null, 'client');
+        // $mform->addRule('create_password', get_string('error_password_regex', 'block_rumbletalk_chat'), 'regex', '^[^,]{6,12}$', 'client');
         $mform->setType('create_password', PARAM_TEXT);
 
-        // $mform->addElement('button', 'create_button', get_string('create_button', 'block_rumbletalk'));
+        // $mform->addElement('button', 'create_button', get_string('create_button', 'block_rumbletalk_chat'));
 
         $this->add_action_buttons();
     }
