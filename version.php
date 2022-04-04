a7641d1c4acbc955f3e250bb9c58743a786304b5
Jeirod Abogado <33385627+jeirod@users.noreply.github.com>
2022-04-04 16:21:06 +0800

    docs: updated version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_rumbletalk_chat';
-$plugin->version = 2021072400;
+$plugin->version = 2022040400;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.1';
